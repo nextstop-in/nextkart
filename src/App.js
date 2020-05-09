6ce1637ce1273202bc846363b94eb4b4ce5efda9
nextstop-in <shanu801304@gmail.com>
2020-05-09 21:05:30 +0530

    added withRouter() HOC to navigate into menu-items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
+const HatsPage = () =>{
+  return(<div>
+    
+    <h1>Hats Page</h1>
+    </div>)
+}
+
 const App = ()=>{
 
   return (<div>
-  <Homepage/> 
-  </div>)
+           <Route  exact path='/' component={Homepage}/>
+           <Route exact path='/hats' component={HatsPage}/>  
+        </div>)
 }
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ constructor(){
 
 render(){
     return(
-        <div className='directory-menu'>{this.state.sections.map(({id,title,imageUrl,size})=>(
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+        <div className='directory-menu'>{this.state.sections.map(({id, ...othersectionprops})=>(
+            <MenuItem key={id} {...othersectionprops} />
         ))} </div>
     )
 }
