deb5784bc588baa346287cdf94347a1185517bff
nextstop-in <shanu801304@gmail.com>
2020-05-10 20:28:47 +0530

    created shop component to show preview of items

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 
 import './App.css';
+import ShopPage from './pages/shop/shop.component'
 import Homepage from './pages/homepage/homepage.component';
 
-const HatsPage = () =>{
-  return(<div>
-    
-    <h1>Hats Page</h1>
-    </div>)
-}
 
 const App = ()=>{
 
   return (<div>
            <Route  exact path='/' component={Homepage}/>
-           <Route exact path='/hats' component={HatsPage}/>  
+           
+           <Route exact path='/shop' component={ShopPage}/>  
         </div>)
 }
 export default App;
